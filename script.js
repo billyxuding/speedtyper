855c7a95fac37ab9b90f51198fc261a3675d2921
Billy Ding <billyxuding@gmail.com>
2021-03-02 21:53:59 -0800

    created functions that get random quotes from an api

--- script.js
+++ script.js
@@ -5,3 +5,18 @@
 // time-left
 // score
 // high-score
+
+var randomQuotesURL = "http://api.quotable.io/random";
+
+function getRandomQuote() {
+    return fetch(randomQuotesURL)
+        .then(response => response.json())
+        .then(data => data.content)
+}
+
+async function getNextQuote() {
+    var quote = await getRandomQuote()
+    console.log(quote)
+}
+
+getNextQuote()
