1380052a4efc0e68e4fa65fddd9af1593cc9778a
Billy Ding <billyxuding@gmail.com>
2021-03-11 20:08:10 -0800

    added semicolons

--- script.js
+++ script.js
@@ -1,59 +1,59 @@
-let TIME_LIMIT = 15
-let SECONDS_REMAINING = TIME_LIMIT
-let SCORE = 0
+let TIME_LIMIT = 15;
+let SECONDS_REMAINING = TIME_LIMIT;
+let SCORE = 0;
 
 function getQuote() {
     $.get("https://api.quotable.io/random", function(response) {
-        const quoteCharacters = response.content.split("")
-        $("#quote").html(null)
-        $("#message").text(null)
+        const quoteCharacters = response.content.split("");
+        $("#quote").html(null);
+        $("#message").text(null);
         for (character of quoteCharacters) {
             $("#quote").append("<span>" + character + "</span>");
         }
-        $("#input").val(null)
-        $("#input").focus()
-        $("#score").text(SCORE)
-        SECONDS_REMAINING = TIME_LIMIT + 1
-    }, "json")
+        $("#input").val(null);
+        $("#input").focus();
+        $("#score").text(SCORE);
+        SECONDS_REMAINING = TIME_LIMIT + 1;
+    }, "json");
 }
 
 function countdown() {
     if (SECONDS_REMAINING > 0) {
-        SECONDS_REMAINING--
+        SECONDS_REMAINING--;
     } else {
-        $("#message").text("Game Over!")
+        $("#message").text("Game Over!");
     }
-    $("#time-left").text(SECONDS_REMAINING)
+    $("#time-left").text(SECONDS_REMAINING);
 }
 
-$(".time-limit").text(TIME_LIMIT)
-getQuote()
-setInterval(countdown, 1000)
+$(".time-limit").text(TIME_LIMIT);
+getQuote();
+setInterval(countdown, 1000);
 
 $("#input").on("input", function() {
-    let typedCorrect = true
+    let typedCorrect = true;
     $("#quote span").each(function(index) {
-        const inputCharacters = $("#input").val().split("")
+        const inputCharacters = $("#input").val().split("");
         if (inputCharacters[index] == undefined) {
-            $(this).removeClass("text-danger")
-            $(this).removeClass("text-success")
-            typedCorrect = false
+            $(this).removeClass("text-danger");
+            $(this).removeClass("text-success");
+            typedCorrect = false;
         } else if (inputCharacters[index] === $(this).text()) {
-            $(this).removeClass("text-danger")
-            $(this).addClass("text-success")
+            $(this).removeClass("text-danger");
+            $(this).addClass("text-success");
         } else {
-            $(this).removeClass("text-success")
-            $(this).addClass("text-danger")
-            typedCorrect = false
+            $(this).removeClass("text-success");
+            $(this).addClass("text-danger");
+            typedCorrect = false;
         }
-    })
+    });
     if (typedCorrect && SECONDS_REMAINING > 0) {
-        SCORE++
-        getQuote()
+        SCORE++;
+        getQuote();
     }
-})
+});
 
 $("#new-game").click(function() {
-    SCORE = 0
-    getQuote()
-})
+    SCORE = 0;
+    getQuote();
+});
